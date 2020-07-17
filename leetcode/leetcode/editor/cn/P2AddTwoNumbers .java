371027ebec22676c095283dc669fcc40eeec4567
lingjing <lingjing@longfor.com>
2020-07-17 18:49:05 +0800

    2020/07/17

--- leetcode/leetcode/editor/cn/P2AddTwoNumbers .java
+++ leetcode/leetcode/editor/cn/P2AddTwoNumbers .java
@@ -15,7 +15,7 @@
 
 package leetcode.editor.cn;
 //Java：两数相加
-class P2AddTwoNumbers{
+public class P2AddTwoNumbers{
     public static void main(String[] args) {
         Solution solution = new P2AddTwoNumbers().new Solution();
         // TO TEST
@@ -64,34 +64,69 @@ public static class ListNode {
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(0);
+        ListNode head = new ListNode(0);
         ListNode cur = new ListNode(0);
-        pre.next = cur;
+        head.next = cur;
         while (l1 != null && l2 != null) {
             ListNode next = new ListNode(0);
             if (cur.val + l1.val + l2.val < 10) {
                 cur.val = cur.val + l1.val + l2.val;
-                cur.next = next;
+                // 如果两个字数最高位相加小于10， 最高位的0 不应该创建
+                if (l1.next == null && l2.next == null) {
+                    // 这里其实就是判断连个数相等， 然后需要把l1, l2进入next， 为接下来的事情做准备
+                    l1 = l1.next;
+                    l2 = l2.next;
+                    break;
+                }
             } else {
                 next.val = 1;
                 cur.val = cur.val + l1.val + l2.val - 10;
-                cur.next = next;
+                // 现在连个数字之和，大于10， 一定是要进1的
             }
+            cur.next = next;
             l1 = l1.next;
             l2 = l2.next;
             cur = next;
         }
+
+        // 如果两个列表个数不相当， 还需要进行处理， 两个道理是一样的， 必然会有一个是忽略不计的
         while(l1 != null) {
-            cur.val = l1.val;
-            cur = cur.next;
+            ListNode next = new ListNode(0);
+            // 考虑到上两位数进1, l1剩余的开头正好是9， 还需要进1
+            if (cur.val + l1.val < 10) {
+                cur.val = cur.val + l1.val;
+                if (l1.next == null) {
+                    break;
+                }
+            } else {
+                next.val = 1;
+                cur.val = cur.val + l1.val - 10;
+            }
+
+            cur.next = next;
             l1 = l1.next;
+            cur = next;
         }
         while(l2 != null) {
-            cur.val = l2.val;
-            cur = cur.next;
+            ListNode next = new ListNode(0);
+            // 考虑到上两位数进1, l1剩余的开头正好是9， 还需要进1
+            if (cur.val + l2.val < 10) {
+                cur.val = cur.val + l2.val;
+                if (l2.next == null) {
+                    break;
+                }
+            } else {
+                next.val = 1;
+                cur.val = cur.val + l2.val - 10;
+            }
+
+            cur.next = next;
             l2 = l2.next;
+            cur = next;
         }
-        return pre.next;
+
+        //最后
+        return head.next;
 
     }
 }
